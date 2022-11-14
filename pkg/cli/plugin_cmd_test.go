cbcca066988b6e3718ee8cb8caed9b78820f9d06
Vui Lam <vui@vmware.com>
2022-11-14 08:38:09 -0800

    Construct cobra Command from PluginInfo
    - Introduce PluginInfo to encapsulate metadata associated with an
      installed plugin. More will be added to it as additional plugin
      management functionality is introduced.
    - Use said PluginInfo data to construct cobra commands to be added to
      the CLI command tree.

---
+++ pkg/cli/plugin_cmd_test.go
@@ -0,0 +1,54 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/vmware-tanzu/tanzu-plugin-runtime/plugin"
+)
+
+func TestGetPluginCmd(t *testing.T) {
+	assert := assert.New(t)
+
+	dir, err := os.MkdirTemp("", "tanzu-cli-getcmd")
+	assert.Nil(err)
+	defer os.RemoveAll(dir)
+
+	path, err := setupFakePlugin(dir, "fakefoo")
+	assert.Nil(err)
+
+	pi := &PluginInfo{
+		Name:             "fakefoo",
+		Description:      "Fake foo",
+		Group:            plugin.SystemCmdGroup,
+		Aliases:          []string{"ff"},
+		InstallationPath: path,
+	}
+	cmd := GetPluginCmd(pi)
+
+	err = cmd.Execute()
+	assert.Equal(cmd.Name(), pi.Name)
+	assert.Equal(cmd.Short, pi.Description)
+	assert.Equal(cmd.Aliases, pi.Aliases)
+	assert.Nil(err)
+}
+
+func setupFakePlugin(dir string, pluginName string) (string, error) {
+	filePath := filepath.Join(dir, "fakefoo")
+
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	fmt.Fprintf(f, "#!/bin/bash\n\necho hello\n")
+	return filePath, nil
+}
