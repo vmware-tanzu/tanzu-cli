cc3076a554fff4e61d7d581b25a87cf716e3bfa3
Vui Lam <vui@vmware.com>
2022-12-05 16:51:49 -0800

    Introduce plugin catalog
    The catalog will record metadata about CLI plugins, including mapping of
    said plugins to the context with which they are associated.
    
    It will be used in an upcoming change by the plugin manager to support
    various plugin lifecycle operations.

---
+++ pkg/catalog/catalog_types.go
@@ -0,0 +1,59 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package catalog
+
+import (
+	"github.com/vmware-tanzu/tanzu-cli/pkg/cli"
+)
+
+// Distro is the Schema for the catalogs API
+// type Distro []string
+
+// PluginAssociation is a set of plugin names and their associated installation paths.
+type PluginAssociation map[string]string
+
+// Add adds plugin entry to the map
+func (pa PluginAssociation) Add(pluginName, installationPath string) {
+	if pa == nil {
+		pa = map[string]string{}
+	}
+	pa[pluginName] = installationPath
+}
+
+// Remove deletes plugin entry from the map
+func (pa PluginAssociation) Remove(pluginName string) {
+	delete(pa, pluginName)
+}
+
+// Get returns installation path for the plugin
+// If plugin doesn't exists in map it will return empty string
+func (pa PluginAssociation) Get(pluginName string) string {
+	return pa[pluginName]
+}
+
+// Map returns associated list of plugins as a map
+func (pa PluginAssociation) Map() map[string]string {
+	return pa
+}
+
+// Catalog is the Schema for the plugin catalog data
+type Catalog struct {
+	// PluginInfos is a list of PluginInfo
+	PluginInfos []*cli.PluginInfo `json:"pluginInfos,omitempty" yaml:"pluginInfos"`
+
+	// IndexByPath of PluginInfos for all installed plugins by installation path.
+	IndexByPath map[string]cli.PluginInfo `json:"indexByPath,omitempty"`
+	// IndeByName of all plugin installation paths by name.
+	IndexByName map[string][]string `json:"indexByName,omitempty"`
+	// StandAlonePlugins is a set of stand-alone plugin installations aggregated across all context types.
+	// Note: Shall be reduced to only those stand-alone plugins that are common to all context types.
+	StandAlonePlugins PluginAssociation `json:"standAlonePlugins,omitempty"`
+	// ServerPlugins links a server and a set of associated plugin installations.
+	ServerPlugins map[string]PluginAssociation `json:"serverPlugins,omitempty"`
+}
+
+// CatalogList contains a list of Catalog
+type CatalogList struct {
+	Items []Catalog `json:"items"`
+}
---
+++ pkg/utils/files.go
@@ -0,0 +1,39 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package utils
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// SaveFile saves the file to the provided path
+// Also creates missing directories if any
+func SaveFile(filePath string, data []byte) error {
+	dirName := filepath.Dir(filePath)
+	if _, serr := os.Stat(dirName); serr != nil {
+		merr := os.MkdirAll(dirName, os.ModePerm)
+		if merr != nil {
+			return merr
+		}
+	}
+
+	err := os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "unable to save file '%s'", filePath)
+	}
+
+	return nil
+}
+
+// PathExists returns true if file/directory exists otherwise returns false
+func PathExists(dir string) bool {
+	_, err := os.Stat(dir)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
