199af137939d2cab0e1f9b8caf8b346f58b60072
Vui Lam <vui@vmware.com>
2022-12-19 22:48:37 -0800

    Add test plugin
    Fixes: #16
    
    The test plugin introduced here is used to install and invoke any
    companion test plugin (test-foo) associated a plugin foo, if test-foo
    exists.
    
    Installing of test plugins with done with the `fetch` command, which now
    only supports installation from local artifacts directory. e.g. via:
    
    tanzu test fetch --local ./artifacts/darwin/amd64/cli
    
    Once installed these test plugins are discovered and presented as
    invocable commands under the `tanzu test plugin` command tree.
    
    Signed-off-by: Vui Lam <vui@vmware.com>

---
+++ cmd/plugin/test/main.go
@@ -0,0 +1,75 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"os"
+
+	"github.com/aunum/log"
+	"github.com/spf13/cobra"
+
+	"github.com/vmware-tanzu/tanzu-plugin-runtime/plugin"
+	"github.com/vmware-tanzu/tanzu-plugin-runtime/plugin/buildinfo"
+
+	"github.com/vmware-tanzu/tanzu-cli/pkg/cli"
+	"github.com/vmware-tanzu/tanzu-cli/pkg/pluginmanager"
+)
+
+var descriptor = plugin.PluginDescriptor{
+	Name:        "test",
+	Description: "Test the CLI",
+	Group:       plugin.AdminCmdGroup,
+	Version:     buildinfo.Version,
+	BuildSHA:    buildinfo.SHA,
+}
+
+var local string
+
+func init() {
+	fetchCmd.Flags().StringVarP(&local, "local", "l", "", "path to local repository")
+	_ = fetchCmd.MarkFlagRequired("local")
+}
+
+func main() {
+	p, err := plugin.NewPlugin(&descriptor)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p.AddCommands(
+		fetchCmd,
+		pluginsCmd,
+	)
+
+	_, installedPlugins, err := pluginmanager.InstalledPlugins()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := range installedPlugins {
+		// Check if test plugin binary installed. If available add a plugin command
+		_, err := os.Stat(cli.TestPluginPathFromPluginPath(installedPlugins[i].InstallationPath))
+		if err != nil {
+			continue
+		}
+		pluginsCmd.AddCommand(cli.GetTestCmdForPlugin(&installedPlugins[i]))
+	}
+
+	if err := p.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var pluginsCmd = &cobra.Command{
+	Use:   "plugin",
+	Short: "Plugin tests",
+}
+
+var fetchCmd = &cobra.Command{
+	Use:   "fetch",
+	Short: "Fetch the plugin tests",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return pluginmanager.InstallPluginsFromLocalSource("all", "", "", local, true)
+	},
+}
