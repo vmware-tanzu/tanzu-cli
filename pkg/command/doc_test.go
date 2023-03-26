95eba4bc4feb2dab607cd12bf70654ecd84d6359
Vui Lam <vui@vmware.com>
2023-03-26 10:02:29 -0700

    Fix generate-all-docs to build core command docs recursively
    Fixes: #95
    
    - ensure all commands rooted at root cmd are recursively processed for their doc
    - added a constructor function to create the command to generate all docs
    - suppress autogenerated footer with timestamp for generated core commands
    - generate set of commands docs for checkin
    - added missing unit test for doc gen
    
    Signed-off-by: Vui Lam <vui@vmware.com>
    (cherry picked from commit 1bf8ee8a53503ce75313e65c0ad3ce6883c499c3)

--- pkg/command/doc_test.go
+++ pkg/command/doc_test.go
@@ -4,11 +4,73 @@ package command
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var (
+	topLevelFileName = "tanzu.md"
+)
+
+func getGeneratedFiles(dir string) ([]string, error) {
+	var files []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, info.Name())
+		}
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return files, err
+}
+
+func topLevelHelp(docsDir string) string {
+	topLevelFilePath := filepath.Join(docsDir, topLevelFileName)
+
+	b, err := os.ReadFile(topLevelFilePath)
+	if err != nil {
+		fmt.Print(err)
+		return ""
+	}
+
+	return string(b)
+}
+
+func TestGenDocs(t *testing.T) {
+	assert := assert.New(t)
+
+	docsDir, err := os.MkdirTemp("", "tanzu-cli-gendocs")
+	assert.Nil(err)
+	defer os.RemoveAll(docsDir)
+
+	rootCmd, err := NewRootCmd()
+	assert.Nil(err)
+	rootCmd.SetArgs([]string{"generate-all-docs", "--docs-dir", docsDir})
+	err = rootCmd.Execute()
+	assert.Nil(err)
+
+	files, err := getGeneratedFiles(docsDir)
+	assert.Nil(err)
+	assert.Contains(files, "tanzu.md")
+	// expects multi level generation of core commands too
+	assert.Contains(files, "tanzu_config.md")
+	assert.Contains(files, "tanzu_config_set.md")
+	assert.Contains(files, "tanzu_plugin_group_search.md")
+
+	// expect only non-hidden commands to be referenced
+	topLevelHelpText := topLevelHelp(docsDir)
+	assert.Contains(topLevelHelpText, "tanzu_context.md")
+	assert.Contains(topLevelHelpText, "[tanzu version]")
+	assert.NotContains(topLevelHelpText, "tanzu_generate")
+}
 
 func TestCompletionGenerateDocs(t *testing.T) {
 	// This is global logic and needs not be tested for each
