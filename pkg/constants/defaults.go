fa9d7099274d2a314c8a4e9f772d2f863fb0c1b0
Anuj Chaudhari <anujc@vmware.com>
2023-03-30 02:16:58 -0700

    Configure staging central plugin repository as default plugin discovery
    This change updates the CLI to use `projects.registry.vmware.com/tanzu_cli_stage/plugins/plugin-inventory:latest`
    as the default central plugin repository.
    
    To override the default central repository, developers can configure `TANZU_CLI_PRE_RELEASE_REPO_IMAGE`.
    However, this is not an officially supported method.

--- pkg/constants/defaults.go
+++ pkg/constants/defaults.go
@@ -15,4 +15,8 @@ const (
 
 	// DefaultBurst is the default maximum burst for throttle for the rest config
 	DefaultBurst = 200
+
+	// TanzuCLIDefaultCentralPluginDiscoveryImage defines the default discovery image
+	// from where the CLI will discover the plugins
+	TanzuCLIDefaultCentralPluginDiscoveryImage = "projects.registry.vmware.com/tanzu_cli_stage/plugins/plugin-inventory:latest"
 )
