40bf6571e925881c9654b79724b6ef56d26baef4
Marc Khouzam <kmarc@vmware.com>
2023-05-12 08:24:35 -0400

    Remove unnecessary operations of "tanzu init"
    The "tanzu init" command was previously required to install the TKG
    plugins through a "plugin sync" (when the CLI was tightly-coupled with
    TKG). Users would be told to call "tanzu init" after installation.
    
    With the Independent CLI, "plugin sync" is no longer used in the same
    way.  Instead users use "plugin install --group <group>"; but since the
    group to use is dependent on the product of interest, this installation
    command cannot be part of a generic "tanzu init" command but has to be
    run manually by the user.
    
    This commit therefore removes the "plugin sync" call from "tanzu init".
    
    Furthermore, a empty read/write of the plugin catalog was being
    performed with the goal to normalize the contents of the catalog. This
    was added when the concept of CLI "targets" was introduced.  However,
    the logic to handle targets has been revamped and this normalization is
    no longer required. It has therefore been removed.
    
    The "tanzu init" command is now a no-op.
    We have decided to keep it for two reasons:
    1- backwards-compatibility (some scripts and even users still call it)
    2- the command may be needed by a future feature so we wanted to avoid
         removing it to re-introduce it some time later.
    
    Signed-off-by: Marc Khouzam <kmarc@vmware.com>

--- pkg/command/init.go
+++ pkg/command/init.go
@@ -8,9 +8,7 @@ import (
 
 	"github.com/vmware-tanzu/tanzu-plugin-runtime/plugin"
 
-	"github.com/vmware-tanzu/tanzu-cli/pkg/catalog"
 	"github.com/vmware-tanzu/tanzu-cli/pkg/cli"
-	"github.com/vmware-tanzu/tanzu-cli/pkg/pluginmanager"
 	"github.com/vmware-tanzu/tanzu-plugin-runtime/log"
 )
 
@@ -26,18 +24,10 @@ var initCmd = &cobra.Command{
 	},
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := initPluginsWithContextAwareCLI()
-		if err != nil {
-			return err
-		}
+		// Currently nothing to initialize.
+		// We are keeping this command as it may become useful
+		// again in the future.
 		log.Success("successfully initialized CLI")
 		return nil
 	},
-}
-
-func initPluginsWithContextAwareCLI() error {
-	if err := catalog.UpdateCatalogCache(); err != nil {
-		return err
-	}
-	return pluginmanager.SyncPlugins()
 }
