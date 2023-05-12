775556867b9ab7069a311d6d0c6b53b77b0933e5
Prem Kumar Kalle <pkalle@vmware.com>
2023-05-12 11:53:41 -0700

    Add support to configure proxy certificate to interact with registry
    - If proxy is configured between CLI and registry which needs a certificate, user can set the environment variable "PROXY_CA_CERT" with base64 value of the proxy cert. The proxy cert would be used while interacting with registry.
    
    Signed-off-by: Prem Kumar Kalle <pkalle@vmware.com>

--- pkg/registry/helpers_test.go
+++ pkg/registry/helpers_test.go
@@ -11,18 +11,19 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/vmware-tanzu/tanzu-cli/pkg/configpaths"
+	"github.com/vmware-tanzu/tanzu-cli/pkg/constants"
 	configlib "github.com/vmware-tanzu/tanzu-plugin-runtime/config"
 	configtypes "github.com/vmware-tanzu/tanzu-plugin-runtime/config/types"
 )
 
-func TestClientConfigHelperSuite(t *testing.T) {
+func TestRegistrySuite(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "pkg/clientconfighelpers suite")
+	RunSpecs(t, "pkg/registry suite")
 }
 
-var _ = Describe("config cert command tests", func() {
+var _ = Describe("GetRegistryCertOptions", func() {
 
-	Describe("config cert add/list command tests", func() {
+	Describe("GetRegistryCertOptions without proxy configuration", func() {
 		var (
 			tanzuConfigFile   *os.File
 			tanzuConfigFileNG *os.File
@@ -113,5 +114,131 @@ var _ = Describe("config cert command tests", func() {
 
 			})
 		})
+
 	})
+
+	Describe("GetRegistryCertOptions with proxy configured", func() {
+		var (
+			tanzuConfigFile    *os.File
+			tanzuConfigFileNG  *os.File
+			caCertDataOpt      string
+			skipCertVerifyOpt  string
+			insecureOpt        string
+			noProxyOpt         string
+			proxyCACertDataB64 string
+			err                error
+		)
+		const (
+			fakeCACertData      = "fake ca cert data"
+			fakeProxyCACertData = "fake proxy ca cert data"
+			testHost            = "test.vmware.com"
+			testHTTPProxyHost   = "192.168.116.1:3128"
+			testHTTPSProxyHost  = "192.168.116.1:3129"
+		)
+
+		BeforeEach(func() {
+			tanzuConfigFile, err = os.CreateTemp("", "config")
+			Expect(err).To(BeNil())
+			os.Setenv("TANZU_CONFIG", tanzuConfigFile.Name())
+
+			tanzuConfigFileNG, err = os.CreateTemp("", "config_ng")
+			Expect(err).To(BeNil())
+			os.Setenv("TANZU_CONFIG_NEXT_GEN", tanzuConfigFileNG.Name())
+
+			os.Setenv("http_proxy", "http://"+testHTTPProxyHost)
+			os.Setenv("https_proxy", "http://"+testHTTPSProxyHost)
+		})
+		AfterEach(func() {
+			os.Unsetenv("TANZU_CONFIG")
+			os.Unsetenv("TANZU_CONFIG_NEXT_GEN")
+			os.RemoveAll(tanzuConfigFile.Name())
+			os.RemoveAll(tanzuConfigFileNG.Name())
+			os.Unsetenv("http_proxy")
+			os.Unsetenv("https_proxy")
+			os.Unsetenv("no_proxy")
+			os.Unsetenv(constants.ProxyCACert)
+		})
+		JustBeforeEach(func() {
+			caCertDataOptB64 := ""
+			if len(caCertDataOpt) > 0 {
+				caCertDataOptB64 = base64.StdEncoding.EncodeToString([]byte(caCertDataOpt))
+			}
+			cert := &configtypes.Cert{
+				Host:           testHost,
+				CACertData:     caCertDataOptB64,
+				SkipCertVerify: skipCertVerifyOpt,
+				Insecure:       insecureOpt,
+			}
+			err := configlib.SetCert(cert)
+			Expect(err).To(BeNil())
+			os.Setenv("no_proxy", noProxyOpt)
+			os.Setenv(constants.ProxyCACert, proxyCACertDataB64)
+		})
+
+		Context("When custom CA cert data is provided for the registry host in the config and proxy cert config(PROXY_CA_CERT) is not provided  ", func() {
+			BeforeEach(func() {
+				caCertDataOpt = fakeCACertData
+			})
+			It("should return success and cert options should have registry cert path updated with registry host CA cert data", func() {
+				certOptions, err := GetRegistryCertOptions(testHost)
+				Expect(err).To(BeNil())
+				Expect(certOptions).ToNot(BeNil())
+
+				regFilePath, err := configpaths.GetRegistryCertFile()
+				Expect(err).To(BeNil())
+				Expect(certOptions.CACertPaths).To(ContainElement(regFilePath))
+				certData, err := os.ReadFile(regFilePath)
+				Expect(err).To(BeNil())
+				Expect(string(certData)).To(Equal(fakeCACertData))
+				Expect(certOptions.SkipCertVerify).To(Equal(false))
+				Expect(certOptions.Insecure).To(Equal(false))
+			})
+		})
+		Context("When custom CA cert data is provided for the registry host in the config and also proxy cert config(PROXY_CA_CERT) is provided", func() {
+			BeforeEach(func() {
+				caCertDataOpt = fakeCACertData
+				proxyCACertDataB64 = base64.StdEncoding.EncodeToString([]byte(fakeProxyCACertData))
+			})
+			It("should return success and cert options should have registry cert path updated with proxy CA cert data", func() {
+
+				certOptions, err := GetRegistryCertOptions(testHost)
+				Expect(err).To(BeNil())
+				Expect(certOptions).ToNot(BeNil())
+
+				regFilePath, err := configpaths.GetRegistryCertFile()
+				Expect(err).To(BeNil())
+				Expect(certOptions.CACertPaths).To(ContainElement(regFilePath))
+				certData, err := os.ReadFile(regFilePath)
+				Expect(err).To(BeNil())
+				Expect(string(certData)).To(Equal(fakeProxyCACertData))
+				Expect(certOptions.SkipCertVerify).To(Equal(false))
+				Expect(certOptions.Insecure).To(Equal(false))
+			})
+		})
+		Context("When cert config is NOT provided for the registry host, but proxy cert config(PROXY_CA_CERT) is provide ", func() {
+			BeforeEach(func() {
+				caCertDataOpt = fakeCACertData
+				proxyCACertDataB64 = base64.StdEncoding.EncodeToString([]byte(fakeProxyCACertData))
+			})
+			It("should return success and cert options should have registry cert path updated with proxy CA cert data", func() {
+				// delete the registry host cert config(as it is added for all the test cases)
+				err := configlib.DeleteCert(testHost)
+				Expect(err).To(BeNil())
+
+				certOptions, err := GetRegistryCertOptions(testHost)
+				Expect(err).To(BeNil())
+				Expect(certOptions).ToNot(BeNil())
+
+				regFilePath, err := configpaths.GetRegistryCertFile()
+				Expect(err).To(BeNil())
+				Expect(certOptions.CACertPaths).To(ContainElement(regFilePath))
+				certData, err := os.ReadFile(regFilePath)
+				Expect(err).To(BeNil())
+				Expect(string(certData)).To(Equal(fakeProxyCACertData))
+				Expect(certOptions.SkipCertVerify).To(Equal(false))
+				Expect(certOptions.Insecure).To(Equal(false))
+			})
+		})
+	})
+
 })
