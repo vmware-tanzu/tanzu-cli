54d90e78db63db66040634db551e3977ff51470e
Panchajanya Mysarla <106101699+mpanchajanya@users.noreply.github.com>
2023-06-05 16:52:05 -0500

    Update the JoinURL method to handle ports (#340)

--- pkg/utils/url.go
+++ pkg/utils/url.go
@@ -35,6 +35,15 @@ func JoinURL(baseURL, relativeURL string) string {
 	// path.Join() takes care of removing or adding any necessary slashes.
 	parsedBaseURL.Path = path.Join(parsedBaseURL.Path, relativeURL)
 
+	// If the Opaque field of the URL isn't empty, the URL path should be joined differently. This is because the net/url package in Go treats a specified port number as an opaque component, thus modifying the way the URL is formatted.
+	// 1. scheme:opaque?query#fragment
+	// 2. scheme://userinfo@host/path?query#fragment
+	// If u.Opaque is non-empty, String uses the first form; otherwise it uses the second form.
+
+	if parsedBaseURL.Opaque != "" {
+		return path.Join(parsedBaseURL.String(), parsedBaseURL.Path)
+	}
+
 	// Return the joined URL as a string
 	return parsedBaseURL.String()
 }
