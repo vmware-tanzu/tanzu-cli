6e89ea8ea0d89139ab74b7dd88204709a4183360
Chandra Pamuluri <cpamuluri@cpamuluri-a01.vmware.com>
2023-08-10 09:15:35 -0500

    change

--- test/e2e/airgapped/plugins.go
+++ test/e2e/airgapped/plugins.go
@@ -12,76 +12,76 @@ const functionality = "functionality"
 // command is implemented. We can use the output of `tanzu plugin group get` command with the original repository
 // and use the created plugin map to do the validation of plugins availability after plugin gets migrated to new repository.
 var pluginsForPGTKG001 = []*framework.PluginInfo{
-	{Name: "cluster", Target: "kubernetes", Version: "v0.0.1", Description: "cluster "+functionality},
-	{Name: "feature", Target: "kubernetes", Version: "v0.0.1", Description: "feature "+functionality},
-	{Name: "kubernetes-release", Target: "kubernetes", Version: "v0.0.1", Description: "kubernetes-release "+functionality},
-	{Name: "management-cluster", Target: "kubernetes", Version: "v0.0.1", Description: "management-cluster "+functionality},
-	{Name: "package", Target: "kubernetes", Version: "v0.0.1", Description: "package "+functionality},
-	{Name: "secret", Target: "kubernetes", Version: "v0.0.1", Description: "secret "+functionality},
+	{Name: "cluster", Target: "kubernetes", Version: "v0.0.1", Description: "cluster " + functionality},
+	{Name: "feature", Target: "kubernetes", Version: "v0.0.1", Description: "feature " + functionality},
+	{Name: "kubernetes-release", Target: "kubernetes", Version: "v0.0.1", Description: "kubernetes-release " + functionality},
+	{Name: "management-cluster", Target: "kubernetes", Version: "v0.0.1", Description: "management-cluster " + functionality},
+	{Name: "package", Target: "kubernetes", Version: "v0.0.1", Description: "package " + functionality},
+	{Name: "secret", Target: "kubernetes", Version: "v0.0.1", Description: "secret " + functionality},
 }
 
 var pluginsForPGTKG999 = []*framework.PluginInfo{
-	{Name: "cluster", Target: "kubernetes", Version: "v9.9.9", Description: "cluster "+functionality},
-	{Name: "feature", Target: "kubernetes", Version: "v9.9.9", Description: "feature "+functionality},
-	{Name: "kubernetes-release", Target: "kubernetes", Version: "v9.9.9", Description: "kubernetes-release "+functionality},
-	{Name: "management-cluster", Target: "kubernetes", Version: "v9.9.9", Description: "management-cluster "+functionality},
-	{Name: "package", Target: "kubernetes", Version: "v9.9.9", Description: "package "+functionality},
-	{Name: "secret", Target: "kubernetes", Version: "v9.9.9", Description: "secret "+functionality},
+	{Name: "cluster", Target: "kubernetes", Version: "v9.9.9", Description: "cluster " + functionality},
+	{Name: "feature", Target: "kubernetes", Version: "v9.9.9", Description: "feature " + functionality},
+	{Name: "kubernetes-release", Target: "kubernetes", Version: "v9.9.9", Description: "kubernetes-release " + functionality},
+	{Name: "management-cluster", Target: "kubernetes", Version: "v9.9.9", Description: "management-cluster " + functionality},
+	{Name: "package", Target: "kubernetes", Version: "v9.9.9", Description: "package " + functionality},
+	{Name: "secret", Target: "kubernetes", Version: "v9.9.9", Description: "secret " + functionality},
 }
 
 //nolint:dupl
 var pluginsForPGTMC001 = []*framework.PluginInfo{
-	{Name: "account", Target: "mission-control", Version: "v0.0.1", Description: "account "+functionality},
-	{Name: "apply", Target: "mission-control", Version: "v0.0.1", Description: "apply "+functionality},
-	{Name: "audit", Target: "mission-control", Version: "v0.0.1", Description: "audit "+functionality},
-	{Name: "cluster", Target: "mission-control", Version: "v0.0.1", Description: "cluster "+functionality},
-	{Name: "clustergroup", Target: "mission-control", Version: "v0.0.1", Description: "clustergroup "+functionality},
-	{Name: "data-protection", Target: "mission-control", Version: "v0.0.1", Description: "data-protection "+functionality},
-	{Name: "ekscluster", Target: "mission-control", Version: "v0.0.1", Description: "ekscluster "+functionality},
-	{Name: "events", Target: "mission-control", Version: "v0.0.1", Description: "events "+functionality},
-	{Name: "iam", Target: "mission-control", Version: "v0.0.1", Description: "iam "+functionality},
-	{Name: "inspection", Target: "mission-control", Version: "v0.0.1", Description: "inspection "+functionality},
-	{Name: "integration", Target: "mission-control", Version: "v0.0.1", Description: "integration "+functionality},
-	{Name: "management-cluster", Target: "mission-control", Version: "v0.0.1", Description: "management-cluster "+functionality},
-	{Name: "policy", Target: "mission-control", Version: "v0.0.1", Description: "policy "+functionality},
-	{Name: "workspace", Target: "mission-control", Version: "v0.0.1", Description: "workspace "+functionality},
-	{Name: "helm", Target: "mission-control", Version: "v0.0.1", Description: "helm "+functionality},
-	{Name: "secret", Target: "mission-control", Version: "v0.0.1", Description: "secret "+functionality},
-	{Name: "continuousdelivery", Target: "mission-control", Version: "v0.0.1", Description: "continuousdelivery "+functionality},
-	{Name: "tanzupackage", Target: "mission-control", Version: "v0.0.1", Description: "tanzupackage "+functionality},
+	{Name: "account", Target: "mission-control", Version: "v0.0.1", Description: "account " + functionality},
+	{Name: "apply", Target: "mission-control", Version: "v0.0.1", Description: "apply " + functionality},
+	{Name: "audit", Target: "mission-control", Version: "v0.0.1", Description: "audit " + functionality},
+	{Name: "cluster", Target: "mission-control", Version: "v0.0.1", Description: "cluster " + functionality},
+	{Name: "clustergroup", Target: "mission-control", Version: "v0.0.1", Description: "clustergroup " + functionality},
+	{Name: "data-protection", Target: "mission-control", Version: "v0.0.1", Description: "data-protection " + functionality},
+	{Name: "ekscluster", Target: "mission-control", Version: "v0.0.1", Description: "ekscluster " + functionality},
+	{Name: "events", Target: "mission-control", Version: "v0.0.1", Description: "events " + functionality},
+	{Name: "iam", Target: "mission-control", Version: "v0.0.1", Description: "iam " + functionality},
+	{Name: "inspection", Target: "mission-control", Version: "v0.0.1", Description: "inspection " + functionality},
+	{Name: "integration", Target: "mission-control", Version: "v0.0.1", Description: "integration " + functionality},
+	{Name: "management-cluster", Target: "mission-control", Version: "v0.0.1", Description: "management-cluster " + functionality},
+	{Name: "policy", Target: "mission-control", Version: "v0.0.1", Description: "policy " + functionality},
+	{Name: "workspace", Target: "mission-control", Version: "v0.0.1", Description: "workspace " + functionality},
+	{Name: "helm", Target: "mission-control", Version: "v0.0.1", Description: "helm " + functionality},
+	{Name: "secret", Target: "mission-control", Version: "v0.0.1", Description: "secret " + functionality},
+	{Name: "continuousdelivery", Target: "mission-control", Version: "v0.0.1", Description: "continuousdelivery " + functionality},
+	{Name: "tanzupackage", Target: "mission-control", Version: "v0.0.1", Description: "tanzupackage " + functionality},
 }
 
 //nolint:dupl
 var pluginsForPGTMC999 = []*framework.PluginInfo{
-	{Name: "account", Target: "mission-control", Version: "v9.9.9", Description: "account "+functionality},
-	{Name: "apply", Target: "mission-control", Version: "v9.9.9", Description: "apply "+functionality},
-	{Name: "audit", Target: "mission-control", Version: "v9.9.9", Description: "audit "+functionality},
-	{Name: "cluster", Target: "mission-control", Version: "v9.9.9", Description: "cluster "+functionality},
-	{Name: "clustergroup", Target: "mission-control", Version: "v9.9.9", Description: "clustergroup "+functionality},
-	{Name: "data-protection", Target: "mission-control", Version: "v9.9.9", Description: "data-protection "+functionality},
-	{Name: "ekscluster", Target: "mission-control", Version: "v9.9.9", Description: "ekscluster "+functionality},
-	{Name: "events", Target: "mission-control", Version: "v9.9.9", Description: "events "+functionality},
-	{Name: "iam", Target: "mission-control", Version: "v9.9.9", Description: "iam "+functionality},
-	{Name: "inspection", Target: "mission-control", Version: "v9.9.9", Description: "inspection "+functionality},
-	{Name: "integration", Target: "mission-control", Version: "v9.9.9", Description: "integration "+functionality},
-	{Name: "management-cluster", Target: "mission-control", Version: "v9.9.9", Description: "management-cluster "+functionality},
-	{Name: "policy", Target: "mission-control", Version: "v9.9.9", Description: "policy "+functionality},
-	{Name: "workspace", Target: "mission-control", Version: "v9.9.9", Description: "workspace "+functionality},
-	{Name: "helm", Target: "mission-control", Version: "v9.9.9", Description: "helm "+functionality},
-	{Name: "secret", Target: "mission-control", Version: "v9.9.9", Description: "secret "+functionality},
-	{Name: "continuousdelivery", Target: "mission-control", Version: "v9.9.9", Description: "continuousdelivery "+functionality},
-	{Name: "tanzupackage", Target: "mission-control", Version: "v9.9.9", Description: "tanzupackage "+functionality},
+	{Name: "account", Target: "mission-control", Version: "v9.9.9", Description: "account " + functionality},
+	{Name: "apply", Target: "mission-control", Version: "v9.9.9", Description: "apply " + functionality},
+	{Name: "audit", Target: "mission-control", Version: "v9.9.9", Description: "audit " + functionality},
+	{Name: "cluster", Target: "mission-control", Version: "v9.9.9", Description: "cluster " + functionality},
+	{Name: "clustergroup", Target: "mission-control", Version: "v9.9.9", Description: "clustergroup " + functionality},
+	{Name: "data-protection", Target: "mission-control", Version: "v9.9.9", Description: "data-protection " + functionality},
+	{Name: "ekscluster", Target: "mission-control", Version: "v9.9.9", Description: "ekscluster " + functionality},
+	{Name: "events", Target: "mission-control", Version: "v9.9.9", Description: "events " + functionality},
+	{Name: "iam", Target: "mission-control", Version: "v9.9.9", Description: "iam " + functionality},
+	{Name: "inspection", Target: "mission-control", Version: "v9.9.9", Description: "inspection " + functionality},
+	{Name: "integration", Target: "mission-control", Version: "v9.9.9", Description: "integration " + functionality},
+	{Name: "management-cluster", Target: "mission-control", Version: "v9.9.9", Description: "management-cluster " + functionality},
+	{Name: "policy", Target: "mission-control", Version: "v9.9.9", Description: "policy " + functionality},
+	{Name: "workspace", Target: "mission-control", Version: "v9.9.9", Description: "workspace " + functionality},
+	{Name: "helm", Target: "mission-control", Version: "v9.9.9", Description: "helm " + functionality},
+	{Name: "secret", Target: "mission-control", Version: "v9.9.9", Description: "secret " + functionality},
+	{Name: "continuousdelivery", Target: "mission-control", Version: "v9.9.9", Description: "continuousdelivery " + functionality},
+	{Name: "tanzupackage", Target: "mission-control", Version: "v9.9.9", Description: "tanzupackage " + functionality},
 }
 
 var pluginsNotInAnyPG999 = []*framework.PluginInfo{
-	{Name: "isolated-cluster", Target: "global", Version: "v9.9.9", Description: "isolated-cluster "+functionality},
-	{Name: "pinniped-auth", Target: "global", Version: "v9.9.9", Description: "pinniped-auth "+functionality},
+	{Name: "isolated-cluster", Target: "global", Version: "v9.9.9", Description: "isolated-cluster " + functionality},
+	{Name: "pinniped-auth", Target: "global", Version: "v9.9.9", Description: "pinniped-auth " + functionality},
 }
 
 var essentialPlugins = []*framework.PluginInfo{
-	{Name: "telemetry", Target: "global", Version: "v9.9.9", Description: "telemetry "+functionality},
+	{Name: "telemetry", Target: "global", Version: "v9.9.9", Description: "telemetry " + functionality},
 }
 
 var pluginsNotInAnyPGAndUsingSha = []*framework.PluginInfo{
-	{Name: "plugin-with-sha", Target: "global", Version: "v9.9.9", Description: "plugin-with-sha "+functionality},
+	{Name: "plugin-with-sha", Target: "global", Version: "v9.9.9", Description: "plugin-with-sha " + functionality},
 }
