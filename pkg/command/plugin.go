900553a54490afe76c58599feff0a829ec70966f
Anuj Chaudhari <anujc@vmware.com>
2023-10-19 10:20:51 -0700

    Update log messages to use Uninstall instead of delete

--- pkg/command/plugin.go
+++ pkg/command/plugin.go
@@ -89,7 +89,7 @@ func newPluginCmd() *cobra.Command {
 	installPluginCmd.Flags().StringVarP(&version, "version", "v", cli.VersionLatest, "version of the plugin")
 	utils.PanicOnErr(installPluginCmd.RegisterFlagCompletionFunc("version", completePluginVersions))
 
-	deletePluginCmd.Flags().BoolVarP(&forceDelete, "yes", "y", false, "delete the plugin without asking for confirmation")
+	deletePluginCmd.Flags().BoolVarP(&forceDelete, "yes", "y", false, "uninstall the plugin without asking for confirmation")
 
 	completeTargets := func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		return []string{compGlobalTarget, compK8sTarget, compTMCTarget}, cobra.ShellCompDirectiveNoFileComp
@@ -377,9 +377,9 @@ func newDeletePluginCmd() *cobra.Command {
 			}
 
 			if pluginName == cli.AllPlugins {
-				log.Successf("successfully deleted all plugins of target '%s'", target)
+				log.Successf("successfully uninstalled all plugins of target '%s'", target)
 			} else {
-				log.Successf("successfully deleted plugin '%s'", pluginName)
+				log.Successf("successfully uninstalled plugin '%s'", pluginName)
 			}
 			return nil
 		},
