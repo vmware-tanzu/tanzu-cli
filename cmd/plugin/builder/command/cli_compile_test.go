758172576c813976b1a17e83697d36bb6ad88029
Anuj Chaudhari <anujc@vmware.com>
2024-01-11 11:56:40 -0800

    Automatically merge the plugin_group_manifest if exists when building plugins
    Before this change, when user tries to build plugins individually the
    content of the plugin_group_manifest.yaml file was getting overwritten
    with each plugin build command.
    
    After this change, if the plugin_group_manifest.yaml file already exists
    under the artifacts directory, while building a new plugin with merge
    the plugin information into the existing plugin_group_manifest.yaml
    file. And if user is building a plugin which exists in the
    plugin_group_manifest.yaml file with different version, the content of
    the plugin_group_manifest.yaml will get updated with newer version of
    the plugin.

--- cmd/plugin/builder/command/cli_compile_test.go
+++ cmd/plugin/builder/command/cli_compile_test.go
@@ -94,3 +94,105 @@ func TestMergePluginManifest(t *testing.T) {
 		assert.Equal(foundPlugin.Versions, plugin.Versions)
 	}
 }
+
+func TestMergePluginGroupManifest(t *testing.T) {
+	assert := assert.New(t)
+
+	basePGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v1.1.1",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v4.0.0",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin3",
+					Target:          "target-baz",
+					IsContextScoped: false,
+				},
+				Version: "v3.0.0",
+			},
+		},
+	}
+
+	incomingPGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: false,
+				},
+				Version: "v1.0.0",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-bar",
+					IsContextScoped: false,
+				},
+				Version: "v2.0.0",
+			},
+		},
+	}
+
+	expectedMergedPGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v1.1.1",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin3",
+					Target:          "target-baz",
+					IsContextScoped: false,
+				},
+				Version: "v3.0.0",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v4.0.0",
+			},
+			cli.PluginNameTargetScopeVersion{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-bar",
+					IsContextScoped: false,
+				},
+				Version: "v2.0.0",
+			},
+		},
+	}
+
+	mergedManifest := mergePluginGroupManifest(basePGManifest, incomingPGManifest)
+
+	for _, plugin := range expectedMergedPGManifest.Plugins {
+		foundPlugin := findpluginInPluginGroupManifest(mergedManifest, plugin)
+		assert.NotNil(foundPlugin, "expected plugin:%v, target:%v not found", plugin.Name, plugin.Target)
+		assert.Equal(foundPlugin.Name, plugin.Name)
+		assert.Equal(foundPlugin.Target, plugin.Target)
+		assert.Equal(foundPlugin.IsContextScoped, plugin.IsContextScoped)
+		assert.Equal(foundPlugin.Version, plugin.Version)
+	}
+}
