69ee8212953ab9763323c36431d364b698ad095e
Prem Kumar Kalle <pkalle@vmware.com>
2024-01-12 00:05:01 -0800

    Add support for interactive login on terminal based hosts(without browser) for tanzu context
    - Add support for interactive login on terminal based hosts for tanzu context. CLI would prompt the authorization URL so that user can open the URL in the host having browser and then copy the authcode manually to complete the login.
    - User can choose the local listener port for callback URLs during OAuth authorization flow by setting the TANZU_CLI_OAUTH_LOCAL_LISTENER_PORT environment variable with port number user want to use on the local host.
    
    Signed-off-by: Prem Kumar Kalle <pkalle@vmware.com>

--- go.mod
+++ go.mod
@@ -47,6 +47,7 @@ require (
 	golang.org/x/mod v0.12.0
 	golang.org/x/oauth2 v0.8.0
 	golang.org/x/sync v0.3.0
+	golang.org/x/term v0.15.0
 	google.golang.org/grpc v1.56.3
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -218,7 +219,6 @@ require (
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
--- pkg/auth/csp/tanzu_test.go
+++ pkg/auth/csp/tanzu_test.go
@@ -4,14 +4,20 @@
 package csp
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/oauth2"
 )
+
+const fakeIssuerURL = "https://fake.issuer.com"
 
 func TestHandleTokenRefresh(t *testing.T) {
 	// Mock HTTP server for token refresh
@@ -81,7 +87,6 @@ func TestGetOrgNameFromOrgID(t *testing.T) {
 func TestGetAuthCodeURL_validResponse(t *testing.T) {
 	assert := assert.New(t)
 	var err error
-	fakeIssuerURL := "https://fake.issuer.com"
 	h := &cspLoginHandler{
 		oauthConfig: &oauth2.Config{
 			RedirectURL: fakeIssuerURL,
@@ -117,4 +122,126 @@ func TestGetAuthCodeURL_validResponse(t *testing.T) {
 	assert.Equal(u.Query().Get("client_id"), tanzuCLIClientID)
 	assert.Equal(u.Query().Get("orgId"), "fake-org-id")
 	assert.Equal(u.Query().Get("code_challenge_method"), "S256")
+}
+
+func TestGetTokenUsingAuthCode(t *testing.T) {
+	// Mock HTTP server for token refresh
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, _ := io.ReadAll(r.Body)
+		if strings.Contains(string(body), "code=valid_auth_code") {
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte(`{"access_token": "fake-access-token", "refresh_token": "fake-refresh-token", "expires_in": 3600, "id_token": "fake-id-token"}`))
+			return
+		}
+		http.Error(w, "invalid_auth_code_fake_error", http.StatusBadRequest)
+	}))
+	defer server.Close()
+
+	// Mock the necessary components
+	h := &cspLoginHandler{
+		oauthConfig: &oauth2.Config{
+			RedirectURL: fakeIssuerURL,
+			ClientID:    tanzuCLIClientID,
+			Endpoint: oauth2.Endpoint{
+				TokenURL: server.URL,
+			},
+		},
+	}
+
+	// Test with a valid auth code
+	token, err := h.getTokenUsingAuthCode(context.TODO(), "valid_auth_code")
+	if err != nil {
+		t.Errorf("getTokenUsingAuthCode returned an unexpected error: %v", err)
+	}
+
+	if token == nil || token.Extra("id_token").(string) == "" {
+		t.Error("getTokenUsingAuthCode did not return the expected token")
+	}
+	// Test with an invalid auth code
+	_, err = h.getTokenUsingAuthCode(context.TODO(), "invalid_auth_code")
+	if err == nil || !strings.Contains(err.Error(), "invalid_auth_code_fake_error") {
+		t.Error("getTokenUsingAuthCode did not return an error for an invalid auth code")
+	}
+}
+
+func TestPromptAndLoginWithAuthCode(t *testing.T) {
+	// Mock HTTP server for token refresh
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, _ := io.ReadAll(r.Body)
+		if strings.Contains(string(body), "code=valid_auth_code") {
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write([]byte(`{"access_token": "fake-access-token", "refresh_token": "fake-refresh-token", "expires_in": 3600, "id_token": "fake-id-token"}`))
+			return
+		}
+		http.Error(w, "invalid_auth_code_fake_error", http.StatusBadRequest)
+	}))
+	defer server.Close()
+
+	// Mock the necessary components
+	h := &cspLoginHandler{
+		tokenExchange:         context.TODO(),
+		tokenExchangeComplete: func() {},
+		oauthConfig: &oauth2.Config{
+			RedirectURL: fakeIssuerURL,
+			ClientID:    tanzuCLIClientID,
+			Endpoint: oauth2.Endpoint{
+				TokenURL: server.URL,
+			},
+		},
+		promptForValue: func(ctx context.Context, promptLabel string, out io.Writer) (string, error) {
+			return "valid_auth_code", nil
+		},
+		isTTY: func(_ int) bool {
+			return true
+		},
+	}
+
+	// Test user providing valid auth code
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	wg := h.promptAndLoginWithAuthCode(ctx, "http://example.com/auth")
+	// Wait for the prompt to finish
+	wg()
+
+	if h.token == nil || h.token.Extra("id_token").(string) == "" {
+		t.Error("promptAndLoginWithAuthCode did not set the token")
+	}
+
+	// Test user providing invalid auth code
+	h.token = nil
+	h.promptForValue = func(ctx context.Context, promptLabel string, out io.Writer) (string, error) {
+		return "invalid_auth_code", nil
+	}
+	wg = h.promptAndLoginWithAuthCode(ctx, "http://example.com/auth")
+	wg()
+
+	if h.token != nil {
+		t.Error("promptAndLoginWithAuthCode set the token using invalid auth code")
+	}
+
+	// Test without a TTY
+	h.token = nil
+	wg = h.promptAndLoginWithAuthCode(ctx, "http://example.com/auth")
+	wg()
+
+	if h.token != nil {
+		t.Error("promptAndLoginWithAuthCode set the token without a TTY")
+	}
+
+	// Test with context canceled while waiting for user input
+	h.token = nil
+	h.promptForValue = func(ctx context.Context, promptLabel string, out io.Writer) (string, error) {
+		time.Sleep(10 * time.Second)
+		return "should_not_reach_here", nil
+	}
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	_ = h.promptAndLoginWithAuthCode(ctx, "http://example.com/auth")
+	ctxCancel()
+
+	if h.token != nil {
+		t.Error("promptAndLoginWithAuthCode set the token with context canceled while waiting for user input")
+	}
 }
