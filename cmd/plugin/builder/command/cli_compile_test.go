a7f610b4c65836705296fd28acf227521cb8c5eb
Anuj Chaudhari <anuj.chaudhari@broadcom.com>
2024-01-16 11:47:30 -0800

    Merge plugin-group manifest automatically when building individual plugins with the `builder` plugin (#644)
    * Automatically merge the plugin_group_manifest if exists when building plugins
    
    Before this change, when user tries to build plugins individually the
    content of the plugin_group_manifest.yaml file was getting overwritten
    with each plugin build command.
    
    After this change, if the plugin_group_manifest.yaml file already exists
    under the artifacts directory, while building a new plugin with merge
    the plugin information into the existing plugin_group_manifest.yaml
    file. And if user is building a plugin which exists in the
    plugin_group_manifest.yaml file with different version, the content of
    the plugin_group_manifest.yaml will get updated with newer version of
    the plugin.

--- cmd/plugin/builder/command/cli_compile_test.go
+++ cmd/plugin/builder/command/cli_compile_test.go
@@ -16,19 +16,19 @@ func TestMergePluginManifest(t *testing.T) {
 
 	baseManifest := cli.Manifest{
 		Plugins: []cli.Plugin{
-			cli.Plugin{
+			{
 				Name:        "plugin1",
 				Target:      "target-foo",
 				Description: "desc-1-foo",
 				Versions:    []string{"v1.1.1"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin2",
 				Target:      "target-foo",
 				Description: "desc-2-foo",
 				Versions:    []string{"v4.0.0"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin3",
 				Target:      "target-baz",
 				Description: "desc-3",
@@ -39,13 +39,13 @@ func TestMergePluginManifest(t *testing.T) {
 
 	incomingManifest := cli.Manifest{
 		Plugins: []cli.Plugin{
-			cli.Plugin{
+			{
 				Name:        "plugin1",
 				Target:      "target-foo",
 				Description: "desc-1-foo",
 				Versions:    []string{"v1.0.0"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin2",
 				Target:      "target-bar",
 				Description: "desc-2",
@@ -56,25 +56,25 @@ func TestMergePluginManifest(t *testing.T) {
 
 	expectedMergedManifest := cli.Manifest{
 		Plugins: []cli.Plugin{
-			cli.Plugin{
+			{
 				Name:        "plugin1",
 				Target:      "target-foo",
 				Description: "desc-1-foo",
 				Versions:    []string{"v1.1.1"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin3",
 				Target:      "target-baz",
 				Description: "desc-3",
 				Versions:    []string{"v3.0.0"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin2",
 				Target:      "target-foo",
 				Description: "desc-2-foo",
 				Versions:    []string{"v4.0.0"},
 			},
-			cli.Plugin{
+			{
 				Name:        "plugin2",
 				Target:      "target-bar",
 				Description: "desc-2",
@@ -92,5 +92,107 @@ func TestMergePluginManifest(t *testing.T) {
 		assert.Equal(foundPlugin.Target, plugin.Target)
 		assert.Equal(foundPlugin.Description, plugin.Description)
 		assert.Equal(foundPlugin.Versions, plugin.Versions)
+	}
+}
+
+func TestMergePluginGroupManifest(t *testing.T) {
+	assert := assert.New(t)
+
+	basePGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v1.1.1",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v4.0.0",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin3",
+					Target:          "target-baz",
+					IsContextScoped: false,
+				},
+				Version: "v3.0.0",
+			},
+		},
+	}
+
+	incomingPGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: false,
+				},
+				Version: "v1.0.0",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-bar",
+					IsContextScoped: false,
+				},
+				Version: "v2.0.0",
+			},
+		},
+	}
+
+	expectedMergedPGManifest := cli.PluginGroupManifest{
+		Plugins: []cli.PluginNameTargetScopeVersion{
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin1",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v1.1.1",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin3",
+					Target:          "target-baz",
+					IsContextScoped: false,
+				},
+				Version: "v3.0.0",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-foo",
+					IsContextScoped: true,
+				},
+				Version: "v4.0.0",
+			},
+			{
+				PluginNameTargetScope: cli.PluginNameTargetScope{
+					Name:            "plugin2",
+					Target:          "target-bar",
+					IsContextScoped: false,
+				},
+				Version: "v2.0.0",
+			},
+		},
+	}
+
+	mergedManifest := mergePluginGroupManifest(basePGManifest, incomingPGManifest)
+
+	for _, plugin := range expectedMergedPGManifest.Plugins {
+		foundPlugin := findpluginInPluginGroupManifest(mergedManifest, plugin)
+		assert.NotNil(foundPlugin, "expected plugin:%v, target:%v not found", plugin.Name, plugin.Target)
+		assert.Equal(foundPlugin.Name, plugin.Name)
+		assert.Equal(foundPlugin.Target, plugin.Target)
+		assert.Equal(foundPlugin.IsContextScoped, plugin.IsContextScoped)
+		assert.Equal(foundPlugin.Version, plugin.Version)
 	}
 }
