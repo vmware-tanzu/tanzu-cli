d66e6db8d2834ecb4f03eb9d6b0591ce29ca3a6b
Anuj Chaudhari <anuj.chaudhari@broadcom.com>
2024-05-03 11:57:00 -0700

    Include new cosign public key to verify the image (#761)

--- pkg/cosignhelper/tanzu_cli_cert.go
+++ pkg/cosignhelper/tanzu_cli_cert.go
@@ -9,3 +9,10 @@ MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEtDdBg1A2wJQ4IH7PP9Ke3IjyT2kO
 YwynjyBpYf9AaL3tbvlgfnN32tZgAgUnb0KNtPW8tcsu/i1QNHCzYc+uKQ==
 -----END PUBLIC KEY-----
 `)
+
+var tanzuCLIPluginDBImageSignPublicKeyOfficialV2 = []byte(`
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEXwvbGFvDXmZ95gngDOqUxkHrhOnT
+jvLHEhX0Nj542gExPUSBRKhh82DN/23qvMXLffg5Za8Q3+H5Xodth3oo4g==
+-----END PUBLIC KEY-----
+`)
